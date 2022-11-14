6967319e841e53ea1294aa3adf0ee68d72480b32
ellen <echan13@dons.usfca.edu>
2022-11-14 13:31:44 -0800

    add signup form to homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,11 @@
 import TripButton from './TripButton';
+import SignupForm from './SignupForm';
 
 export default function HomePage() {
   return (
     <div>
       <h1 className="text-center mt-5">Packd</h1>
+      <SignupForm></SignupForm>
       <TripButton></TripButton>      
     </div>
   );
---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+// import "../css-files/TripAddInput.css"
+
+const SignupForm = () => {
+    const [username, setUsername] = useState("");
+
+    const onSubmitForm = async (e) => {
+        e.preventDefault();
+        try {
+            const body = { username };
+            console.log(body);
+            
+
+            const getResponse = await fetch(`http://localhost:8000/users/username/${username}`);
+            const jsonData = await getResponse.json();
+
+            console.log(jsonData);
+
+            // const postResponse = await fetch("http://localhost:8000/users", {
+            //     method: "POST",
+            //     headers: { "Content-Type": "application/json" },
+            //     body: JSON.stringify(body)
+            // });
+
+            // window.location = `/trips/${user_id}`;
+
+            // console.log(response);
+            // const data = await response.json();
+            // console.log(data);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+    return (
+        <div>
+            <h3 className="text mx-5">Signup</h3>
+            <p style={{ visibility: true ? "visible" : "hidden" }}>Username taken</p>
+            <form className="d-flex mx-5" onSubmit={onSubmitForm}>
+                <input type="text" className="form form-control" value={username} placeholder="Enter username" onChange={e => setUsername(e.target.value)} />
+                <button className="btn btn-primary">Signup</button>
+            </form>
+        </div>
+    );
+};
+
+export default SignupForm;
