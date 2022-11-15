7dba0ce22f1523803b569c6f59583d0f2f74d7f4
ellen <echan13@dons.usfca.edu>
2022-11-14 16:25:06 -0800

    add routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TripPage from './components/trip-page-components/TripPage';
 import HomePage from "./components/HomePage";
 import ListPage from "./components/list-page-components/ListPage";
 import LoginForm from "./components/LoginForm";
+import SignupPage from "./components/SignupPage";
 
 export default class App extends Component {
   render() {
@@ -17,6 +18,7 @@ export default class App extends Component {
           <Route path="/trips/:user_id" element={<TripPage/>} />
           <Route path="/list/:trip_id" element={<ListPage/>}/>
           <Route path="/user-login" element={<LoginForm/>}/>
+          <Route path="/user-signup" element={<SignupPage/>}/>
         </Routes>
       </Router>
     );
